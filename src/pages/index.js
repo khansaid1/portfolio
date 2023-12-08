a1de4027381831f156787903307709a6377dfcd8
khansaid1 <khansaid2223@gmail.com>
2023-12-08 21:26:22 +0500

    update cv
    update cv

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,12 +37,14 @@ export default function Home() {
               Explore my latest projects, showcasing my expertise in React.js and web development.
               </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
-                <Link href="mailto:khansaid2223@gmail.com" 
+                <Link 
+                href={'khansaid.pdf'} 
                 className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light 
                 hover:text-dark border-2 border-solid border-transparent hover:border-dark
                  dark:text-dark dark:bg-light hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
                  md:p-2 md:px-4 md:text-base'
-                target={"_blank"}>Resume</Link>
+                target={"_blank"}
+                download={true}>Resume</Link>
                 <Link href="tel:+923467575455" target={"_blank"} 
                 className='ml-4 text-lg font-medium underline capitalize underline-offset-4 text-dark dark:text-light md:text-base'>
                  +92 346 7575455
